729b2c9f1cd890f51dd7f8156236d1f10c0d20d8
ТвоеИмя <@email.com>
2025-11-20 01:18:59 +0300

    ...

--- StipeProj/settings.py
+++ StipeProj/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-import dj_database_url
-
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
